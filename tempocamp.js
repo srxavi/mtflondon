f3b4ddefdf952872b3cdd0869e07311a72ad3e43
Sara Gozalo <sara89sgm@gmail.com>
2012-05-18 17:26:24 +0100

    playMusic

--- tempocamp.js
+++ tempocamp.js
@@ -1,68 +1,79 @@
 var count = 0;
 var msecsFirst = 0;
 var msecsPrevious = 0;
-var actualBPM=0;
+var actualBPM = 0;
 $("#tapDiv").live("tap", function(event) {
-                  TapForBPM();
-                  });
-
-var timerID = setTimeout("getNewSong()", 5000);
+	TapForBPM();
+});
 
 function ResetCount() {
-    count = 0;
-    $("#Tavg").val("");
-    //document.TAP_DISPLAY.T_TAP.value = "";
-    $("#T_RESET").blur();
+	count = 0;
+	$("#Tavg").val("");
+	//document.TAP_DISPLAY.T_TAP.value = "";
+	$("#T_RESET").blur();
 }
 
 function TapForBPM() {
-    
-    $("#t_wait").blur();
-    timeSeconds = new Date;
-    msecs = timeSeconds.getTime();
-    if ((msecs - msecsPrevious) > 1000 * $("#t_wait").value) {
-        count = 0;
-    }
-    
-    if (count == 0) {
-        $("#Tavg").val("First Beat");
-        //document.TAP_DISPLAY.T_TAP.value = "First Beat";
-        msecsFirst = msecs;
-        count = 1;
-    } else {
-        bpmAvg = 60000 * count / (msecs - msecsFirst);
-        $("#Tavg").val(Math.round(bpmAvg));
-        actualBPM=Math.round(bpmAvg);
-        count++;
-        //document.TAP_DISPLAY.T_TAP.value = count;
-    }
-    msecsPrevious = msecs;
-    return true;
+
+	$("#t_wait").blur();
+	timeSeconds = new Date;
+	msecs = timeSeconds.getTime();
+	if ((msecs - msecsPrevious) > 1000 * $("#t_wait").value) {
+		count = 0;
+	}
+
+	if (count == 0) {
+		$("#Tavg").val("First Beat");
+		//document.TAP_DISPLAY.T_TAP.value = "First Beat";
+		msecsFirst = msecs;
+		count = 1;
+	} else {
+		bpmAvg = 60000 * count / (msecs - msecsFirst);
+		$("#Tavg").val(Math.round(bpmAvg));
+		if ((Math.abs(Math.round(bpmAvg) - actualBPM)) > 15) {
+
+			actualBPM = Math.round(bpmAvg);
+			getNewSong();
+		} else {
+			actualBPM = Math.round(bpmAvg);
+		}
+		count++;
+		//document.TAP_DISPLAY.T_TAP.value = count;
+	}
+	msecsPrevious = msecs;
+	return true;
 }
-
-
 
 function getNewSong() {
-    minVal=actualBPM-5;
-    maxVal=actualBPM+5;
-    var url = 'http://musictechfest:mtflondon2012_@ella.bmat.ws/collections/tags/tags/electronic/similar/collections/bmat/tracks?filter=rhythm.bpm:['+minVal+'%20TO%20'+maxVal+']&format=json&similarity_type=playlist';
-    $.ajax({
-           url: url,
-           dataType: 'jsonp',
-           jsonp: 'callback',
-           jsonpCallback: 'playMusic'
-           });
+
+	minVal = actualBPM - 5;
+	maxVal = actualBPM + 5;
+	var url = 'http://musictechfest:mtflondon2012_@ella.bmat.ws/collections/tags/tags/electronic/similar/collections/bmat/tracks?filter=rhythm.bpm:['
+			+ minVal + '%20TO%20' + maxVal + ']&format=json';
+	$.ajax({
+		url : url,
+		dataType : 'json',
+		type : 'GET',
+		success : function(data) {
+		
+			playMusic(data);
+		}
+	});
+
 }
 
-var playMusic = function(data) {
-    var myObject = eval('(' + data + ')');
-    playAudio(myObject.result[0].entity.metada.location);
-    alert(JSON.stringify(data));
+function playMusic(data) {
+	
+	alert(data.result[0].entity.metada.location);
+	playAudio(myObject.result[0].entity.metada.location);
+
 }
+var my_media = null;
 function playAudio(src) {
-    // Create Media object from src
-    my_media = new Media(src, onSuccess, onError);
-    
-    // Play audio
-    my_media.play();
+	// Create Media object from src
+	alert(src);
+	my_media = new Media(src, onSuccess, onError);
+
+	// Play audio
+	my_media.play();
 }
